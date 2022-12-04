e7d45a299ca830d7e74ac298939761b0b3529a84
M1racle <crazyhl@163.com>
2022-12-04 13:04:51 +0800

    优化 调整 checkbox 的文字和选项框的位置，获取以后可以做成选项

--- resources/views/checkbox.blade.php
+++ resources/views/checkbox.blade.php
@@ -4,8 +4,8 @@
     </label>
     @foreach($options as $optionValue => $option)
         <label class="label cursor-pointer">
-            <input wire:model="value" type="checkbox" value="{{$optionValue}}" class="{{$type == 'toggle' ? 'toggle' : 'checkbox'}} {{$type}}-primary"/>
             <span class="label-text">{{$option}}</span>
+            <input wire:model="value" type="checkbox" value="{{$optionValue}}" class="{{$type == 'toggle' ? 'toggle' : 'checkbox'}} {{$type}}-primary"/>
         </label>
     @endforeach
     <span class="label-text-alt">
