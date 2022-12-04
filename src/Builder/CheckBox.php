187da86b8510c14152748e199e6ad8bda1f85912
M1racle <crazyhl@163.com>
2022-12-04 12:01:41 +0800

    新增 checkbox

---
+++ src/Builder/CheckBox.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CimpleAdmin\Forms\Builder;
+
+use Illuminate\Validation\Rule;
+
+class CheckBox extends Component
+{
+    const COMPONENT_NAME = 'checkbox';
+    private array $options = [];
+    private string $type = 'checkbox';
+
+    public function __construct($property)
+    {
+        parent::__construct($property);
+        $this->required()->accept();
+    }
+
+
+    public function label($label): static
+    {
+        parent::label($label);
+        $this->options = [true => $label];
+
+        return $this;
+    }
+
+    public function toggle(): static
+    {
+        $this->type = 'toggle';
+
+        return $this;
+    }
+
+    public function build(): array
+    {
+        return [
+            'rules' => $this->rules,
+            'property' => $this->property,
+            'label' => $this->label,
+            'options' => $this->options,
+            'value' => $this->value,
+            'type' => $this->type,
+        ];
+    }
+}
