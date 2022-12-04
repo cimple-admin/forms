748b47cf428e30da99eace68b4a8a954840ef301
M1racle <crazyhl@163.com>
2022-12-04 11:51:42 +0800

    优化 增加一处注释

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -14,6 +14,7 @@ class FormServiceProvider extends ServiceProvider
     public function boot(ResponseFactory $response)
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'form');
+        // 这里声明的只是 components 不是 builder 别搞混了
         Livewire::component('input-text', TextInput::class);
         Livewire::component('input-password', PasswordInput::class);
         Livewire::component('checkbox', Checkbox::class);
