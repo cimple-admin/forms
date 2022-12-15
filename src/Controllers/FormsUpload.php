ed13813fd11ea101625025904495ff4640fac2b1
Liang Hao <crazyhl@163.com>
2022-12-15 22:38:05 +0800

    Apply fixes from StyleCI (#23)

--- src/Controllers/FormsUpload.php
+++ src/Controllers/FormsUpload.php
@@ -3,7 +3,6 @@
 namespace CimpleAdmin\Forms\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class FormsUpload
@@ -11,21 +10,16 @@ class FormsUpload
     public function upload(Request $request)
     {
         $uploadSuccess = false;
-        if($request->hasfile('file'))
-        {
-            $chunkIndex  = $request->post('dzchunkindex');
+        if ($request->hasfile('file')) {
+            $chunkIndex = $request->post('dzchunkindex');
             $fileName = $request->post('dzuuid');
-            foreach($request->file('file') as $file)
-            {
+            foreach ($request->file('file') as $file) {
                 if ($chunkIndex == 0) {
-                    $uploadSuccess = Storage::put('upload/' . $fileName . '.pdf' , $file);
+                    $uploadSuccess = Storage::put('upload/'.$fileName.'.pdf', $file);
                 } else {
-                    $uploadSuccess = Storage::append('upload/' . $fileName . '.pdf' , $file, null);
-
+                    $uploadSuccess = Storage::append('upload/'.$fileName.'.pdf', $file, null);
                 }
             }
-
-
         }
         if ($uploadSuccess) {
             return response()->json('success', 200);
