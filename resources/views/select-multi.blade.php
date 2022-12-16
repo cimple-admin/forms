7b6efe81d5d47b1f85d0acc16d7ae0fc43369ead
M1racle <crazyhl@163.com>
2022-12-16 21:02:05 +0800

    优化 样式按需加载这不就来了么

--- resources/views/select-multi.blade.php
+++ resources/views/select-multi.blade.php
@@ -6,11 +6,11 @@
     @endif
 
     <div wire:ignore>
-        <select  wire:ignore.self class="select select-primary w-full"
-                 data-trigger
-                 id="choices-multiple-default"
-                 placeholder="This is a placeholder"
-                 multiple
+        <select wire:ignore.self class="select select-primary w-full"
+                data-trigger
+                id="choices-multiple-default"
+                placeholder="This is a placeholder"
+                multiple
         >
             @foreach($options as $optionValue => $option)
                 <option value="{{$optionValue}}">{{$option}}</option>
@@ -27,26 +27,46 @@
             @enderror
             @enderror
     </span>
-    <script>
-        document.addEventListener('livewire:load', function () {
-            const element = document.querySelector('#choices-multiple-default');
-            const choices = new Choices(element, {
-                allowHTML: true,
-                removeItems: true,
-                removeItemButton: true,
-            });
-            choices.passedElement.element.addEventListener(
-                'change',
-                function (event) {
-                    // do something creative here...
-                    @this.value = choices.getValue(true);
-                },
-                false,
-            );
+    @once
+        @push('style')
+            <link rel="stylesheet" href="{{asset('/vendor/forms/choices/styles/choices.css')}}">
+            <style>
+                .choices__list--multiple .choices__item {
+                    background-color: #0071BC;
+                    border: 1px solid #0071BC;
+                    border-radius: 0px;
+                }
+            </style>
+        @endpush
+        @push('scripts')
+            <script src="{{asset('/vendor/forms/choices/scripts/choices.min.js')}}"></script>
+            <script>
+                document.addEventListener('livewire:load', function () {
+                    const element = document.querySelector('#choices-multiple-default');
+                    const choices = new Choices(element, {
+                        allowHTML: true,
+                        removeItems: true,
+                        removeItemButton: true,
+                    });
+                    choices.passedElement.element.addEventListener(
+                        'change',
+                        function (event) {
+                            // do something creative here...
+                            @this.
+                            value = choices.getValue(true);
+                        },
+                        false,
+                    );
 
-            if (@this.value) {
-                choices.setChoiceByValue(@this.value)
-            }
-        })
-    </script>
+                    if (@this.
+                    value
+                )
+                    {
+                        choices.setChoiceByValue(@this.value
+                    )
+                    }
+                })
+            </script>
+        @endpush
+    @endonce
 </div>
--- resources/views/wang-editor.blade.php
+++ resources/views/wang-editor.blade.php
@@ -13,34 +13,88 @@
             @enderror
          </span>
     </label>
-    <script>
-        document.addEventListener('livewire:load', function () {
-            const editorConfig = {
-                placeholder: 'Type here...',
-                MENU_CONF: {},
-                onChange(editor) {
-                    const html = editor.getHtml();
-                    console.log('editor content', html);
-                    @this.value = html
-                    // 也可以同步到 <textarea>
+    @once
+        @push('style')
+            <link rel="stylesheet" href="{{asset('/vendor/forms/wangeditor/css/style.css')}}">
+            <style>
+                #editor—wrapper {
+                    border: 1px solid #ccc;
+                    z-index: 100; /* 按需定义 */
                 }
-            }
 
-            const editor = createEditor({
-                selector: '#editor-container',
-                html: '<h1>fdas</h1>',
-                config: editorConfig,
-                mode: 'default', // or 'simple'
-            })
+                #toolbar-container {
+                    border-bottom: 1px solid #ccc;
+                }
 
-            const toolbarConfig = {}
+                #editor-container {
+                    height: 500px;
+                }
 
-            const toolbar = createToolbar({
-                editor,
-                selector: '#toolbar-container',
-                config: toolbarConfig,
-                mode: 'default', // or 'simple'
-            })
-        })
-    </script>
+                .w-e-text-container h1 {
+                    font-size: 1.5rem; /* 24px */
+                    line-height: 2rem; /* 32px */
+                    font-weight: 700;
+                }
+
+                .w-e-text-container h2 {
+                    font-size: 1.25rem; /* 20px */
+                    line-height: 1.75rem; /* 28px */
+                    font-weight: 700;
+                }
+
+                .w-e-text-container h3 {
+                    font-size: 1.125rem; /* 18px */
+                    line-height: 1.75rem; /* 28px */
+                    font-weight: 700;
+                }
+
+                .w-e-text-container h4 {
+                    font-size: 1rem; /* 16px */
+                    line-height: 1.5rem; /* 24px */
+                    font-weight: 700;
+                }
+
+                .w-e-text-container h5 {
+                    font-size: 0.875rem; /* 14px */
+                    line-height: 1.25rem; /* 20px */
+                    font-weight: 700;
+                }
+            </style>
+        @endpush
+        @push('scripts')
+            <script src="{{asset('/vendor/forms/wangeditor/index.js')}}"></script>
+            <script>
+                document.addEventListener('livewire:load', function () {
+                    const {createEditor, createToolbar} = window.wangEditor
+                    const editorConfig = {
+                        placeholder: 'Type here...',
+                        MENU_CONF: {},
+                        onChange(editor) {
+                            const html = editor.getHtml();
+                            console.log('editor content', html);
+                            @this.value = html
+                            // 也可以同步到 <textarea>
+                        }
+                    }
+
+                    const editor = createEditor({
+                        selector: '#editor-container',
+                        html: @this.value,
+                        config: editorConfig,
+                        mode: 'default', // or 'simple'
+                    })
+
+                    const toolbarConfig = {}
+
+                    const toolbar = createToolbar({
+                        editor,
+                        selector: '#toolbar-container',
+                        config: toolbarConfig,
+                        mode: 'default', // or 'simple'
+                    })
+                })
+            </script>
+        @endpush
+    @endonce
+
 </div>
