46bbb53ee9666763b0f9570766b66b360ceb0a89
M1racle <crazyhl@163.com>
2023-01-03 14:10:35 +0800

    移除 已经完成的TODO

--- src/Builder/TextInput.php
+++ src/Builder/TextInput.php
@@ -5,10 +5,6 @@ namespace CimpleAdmin\Forms\Builder;
 class TextInput extends Component
 {
     const COMPONENT_NAME = 'input-text';
-    // TODO 这里需要增加一个改变type 的方法，
-    // TODO 除了 password 剩下的都可以在这边通过type 方法改变，
-    // TODO 当然 password 最后也可以考虑到这边一起处理
-
     public string $type = 'text';
 
     public function type($type): static
