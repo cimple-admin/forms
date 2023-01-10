6404332c2a1fb5d5db8df5175dc190bf21712317
M1racle <crazyhl@163.com>
2023-01-10 18:00:11 +0800

    优化 部分文件上传样式

--- resources/views/file-uploader.blade.php
+++ resources/views/file-uploader.blade.php
@@ -1,72 +1,19 @@
 <div class="form-group {{ $inline ? 'row' : '' }}">
     @include('form::base.label')
     <div class="{{ $inline ? 'col-sm-10' : '' }}">
-        <div id="actions" class="row">
-            <div class="col-lg-6">
-                <div class="btn-group w-100">
-                      <span class="btn btn-success col fileinput-button">
-                        <i class="fas fa-plus"></i>
-                        <span>Add files</span>
-                      </span>
-                    <button type="submit" class="btn btn-primary col start">
-                        <i class="fas fa-upload"></i>
-                        <span>Start upload</span>
-                    </button>
-                    <button type="reset" class="btn btn-warning col cancel">
-                        <i class="fas fa-times-circle"></i>
-                        <span>Cancel upload</span>
-                    </button>
-                </div>
-            </div>
-            <div class="col-lg-6 d-flex align-items-center">
-                <div class="fileupload-process w-100">
-                    <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                        <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="table table-striped files" id="previews">
-            <div id="template" class="row mt-2">
-                <div class="col-auto">
-                    <span class="preview"><img src="data:," alt="" data-dz-thumbnail /></span>
-                </div>
-                <div class="col d-flex align-items-center">
-                    <p class="mb-0">
-                        <span class="lead" data-dz-name></span>
-                        (<span data-dz-size></span>)
-                    </p>
-                    <strong class="error text-danger" data-dz-errormessage></strong>
-                </div>
-                <div class="col-4 d-flex align-items-center">
-                    <div class="progress progress-striped active w-100" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                        <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                    </div>
-                </div>
-                <div class="col-auto d-flex align-items-center">
-                    <div class="btn-group">
-                        <button class="btn btn-primary start">
-                            <i class="fas fa-upload"></i>
-                            <span>Start</span>
-                        </button>
-                        <button data-dz-remove class="btn btn-warning cancel">
-                            <i class="fas fa-times-circle"></i>
-                            <span>Cancel</span>
-                        </button>
-                        <button data-dz-remove class="btn btn-danger delete">
-                            <i class="fas fa-trash"></i>
-                            <span>Delete</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div id="testUpload" class="dropzone"></div>
         @include('form::base.errors')
         @include('form::base.hint')
     </div>
     @once
         @push('style')
             <link rel="stylesheet" href="{{asset('/vendor/forms/admin-lte/plugins/dropzone/min/dropzone.min.css')}}">
+            <style>
+                .dropzone {
+                    border: 1px solid rgba(0, 0, 0, .3);
+                    border-radius: 4px;
+                }
+            </style>
         @endpush
         @push('scripts')
             <script src="{{asset('/vendor/forms/admin-lte/plugins/dropzone/min/dropzone.min.js')}}"></script>
@@ -78,13 +25,14 @@
                 Dropzone.autoDiscover = false
 
                 // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-                var previewNode = document.querySelector("#template")
-                previewNode.id = ""
-                var previewTemplate = previewNode.parentNode.innerHTML
-                previewNode.parentNode.removeChild(previewNode)
+                // var previewNode = document.querySelector("#template")
+                // previewNode.id = ""
+                // var previewTemplate = previewNode.parentNode.innerHTML
+                // previewNode.parentNode.removeChild(previewNode)
 
-                var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+                var myDropzone = new Dropzone('#testUpload', { // Make the whole body a dropzone
                     url: @this.uploadUrl, // Set the url
+                    dictDefaultMessage: @this.buttonText,
                     thumbnailWidth: 80,
                     thumbnailHeight: 80,
                     parallelUploads: 20,
@@ -92,10 +40,10 @@
                     forceChunking: true,
                     chunkSize: @this.chunkSize,
                     maxFilesize: @this.maxFileSize,
-                    previewTemplate: previewTemplate,
+                    // previewTemplate: previewTemplate,
                     autoQueue: false, // Make sure the files aren't queued until manually added
-                    previewsContainer: "#previews", // Define the container to display the previews
-                    clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+                    // previewsContainer: "#previews", // Define the container to display the previews
+                    // clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
                 })
 
                 myDropzone.on("addedfile", function (file) {
@@ -118,19 +66,19 @@
                 })
 
                 // Hide the total progress bar when nothing's uploading anymore
-                myDropzone.on("queuecomplete", function(progress) {
+                myDropzone.on("queuecomplete", function (progress) {
                     document.querySelector("#total-progress").style.opacity = "0"
                 })
 
                 // Setup the buttons for all transfers
                 // The "add files" button doesn't need to be setup because the config
                 // `clickable` has already been specified.
-                document.querySelector("#actions .start").onclick = function() {
-                    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-                }
-                document.querySelector("#actions .cancel").onclick = function() {
-                    myDropzone.removeAllFiles(true)
-                }
+                // document.querySelector("#actions .start").onclick = function() {
+                //     myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
+                // }
+                // document.querySelector("#actions .cancel").onclick = function() {
+                //     myDropzone.removeAllFiles(true)
+                // }
                 // DropzoneJS Demo Code End
             })
         </script>
--- src/Builder/FileUploader.php
+++ src/Builder/FileUploader.php
@@ -8,6 +8,7 @@ class FileUploader extends Component
 
     private int $chunkSize; // 分块大小
     private int $maxFileSize; // 最大文件大小
+    private string $buttonText = '点击或拖拽文件到此区域上传';
     private string $uploadUrl;
 
     public function chunkSize($chunkSize): static
@@ -35,12 +36,20 @@ class FileUploader extends Component
         return $this;
     }
 
+    public function buttonMessage($buttonMessage): static
+    {
+        $this->buttonText = $buttonMessage;
+
+        return $this;
+    }
+
     public function build(): array
     {
         $params = parent::build();
         $params['chunkSize'] = $this->chunkSize ?? 5 * 1024 * 1024;
         $params['maxFileSize'] = $this->maxFileSize ?? 200;
         $params['uploadUrl'] = $this->uploadUrl ?? '/cimple-admin/form/file/upload';
+        $params['buttonText'] = $this->buttonText;
 
         return $params;
     }
