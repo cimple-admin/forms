9634f4cc5cd8545bdb0d401a10729625d041c2aa
M1racle <crazyhl@163.com>
2023-01-18 11:28:14 +0800

    优化 部分输入框增加 lazy 修饰

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,7 +1,9 @@
 <div class="form-group {{ $inline ? 'row' : '' }}">
     @include('form::base.label')
     <div class="{{ $inline ? 'col-sm-10' : '' }}">
-        <input type="{{$type}}" name="{{$property}}" wire:model="value" class="form-control {{ $errors->has('value') ? 'is-invalid' : '' }}" id="input-{{$property}}" placeholder="{{$placeHolder}}">
+        <input type="{{$type}}" name="{{$property}}" wire:model.lazy="value"
+               class="form-control {{ $errors->has('value') ? 'is-invalid' : '' }}" id="input-{{$property}}"
+               placeholder="{{$placeHolder}}">
         @include('form::base.errors')
         @include('form::base.hint')
     </div>
--- resources/views/textarea.blade.php
+++ resources/views/textarea.blade.php
@@ -1,7 +1,8 @@
 <div class="form-group {{ $inline ? 'row' : '' }}">
     @include('form::base.label')
     <div class="{{ $inline ? 'col-sm-10' : '' }}">
-        <textarea class="form-control"  rows="{{$rows}}" placeholder="{{$placeHolder}}"></textarea>
+        <textarea class="form-control" wire:model.lazy="value" rows="{{$rows}}"
+                  placeholder="{{$placeHolder}}"></textarea>
         @include('form::base.errors')
         @include('form::base.hint')
     </div>
